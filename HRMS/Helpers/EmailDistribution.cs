91ea8573d6a8a4121f73025fe359410256f62d94
jsnape <jsnape95@gmail.com>
2017-04-06 00:24:00 +0100

    Added email distribution

---
+++ HRMS/Helpers/EmailDistribution.cs
@@ -0,0 +1,31 @@
+﻿using RA.EmailDistribution.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRMS.Helpers
+{
+    public class EmailDistribution
+    {
+        public void SendEmail(string emailTo, string subject)
+        {
+            var email = new EmailDistributionData();
+            email.AddEmail(
+                    "no-reply@hrms.co.uk",
+                    emailTo,
+                    "",
+                    "",
+                    subject,
+                    new List<string>{ },
+                    "Welcome to the HRMS.",
+                    "<html><body><p>Welcome to the HRMS!</p></body></html>",
+                    1,
+                    "",
+                    "",
+                    "",
+                    DateTime.Now
+                );
+        }
+    }
+}
